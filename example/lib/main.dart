50a2f6b65fe5070347be4b311a5dc11b64562c64
Welliton Klein <welliton.fokushima@gmail.com>
2023-09-13 10:14:54 -0300

    feat: criando projeto de exemplo para apoioar nos testes

---
+++ example/lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:nps_plugin/nps_plugin.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NPS Plugin',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.light,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FilledButton(
+          child: const Text('Show NPS'),
+          onPressed: () async {
+            final response = await npsStart(
+              context,
+              npsTitle: 'So far, how likely are you to recommend the ',
+              owner: 'Flutter',
+              feedbackTitle: 'Leave your feedback',
+              showInputPhone: true,
+            );
+
+            if (response == null) return;
+            log(response.toString());
+          },
+        ),
+      ),
+    );
+  }
+}
